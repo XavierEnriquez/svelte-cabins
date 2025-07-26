1d859f54fd593d8f7e2a2a88aa8a56d37f512c09
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-07-26 16:07:52 -0500

    Create +page.svelte

---
+++ src/routes/(Website)/cabins/cabin[cabinId]/+page.svelte
@@ -0,0 +1,8 @@
+<main class="m-8">
+    <div>Cabin Component</div>
+<h2 class="text-5xl font-semibold text-center mb-10 text-accent-400">
+          Reserve {"cabin.name"} today. Pay on arrival.
+        </h2>
+
+        <div>Reservation Component</div>
+</main>
