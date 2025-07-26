b8286ac91d957d0d06b6dce49f6d10b11fdef840
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-07-26 14:05:27 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="m-8">
-<h1 class="font-bold text-3xl mb-8">Welcome to DevKommando's SvelteKit develoment template</h1>
+<h1 class="font-bold text-3xl mb-8">Welcome to Svelte Cabins</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> for the SvelteKit documentation</p>
 
 </div>
