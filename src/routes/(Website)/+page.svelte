4743b2a00dcbedd9256887f7dddb0652436ac2d4
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-07-26 23:05:44 -0500

    Update +page.svelte

--- src/routes/(Website)/+page.svelte
+++ src/routes/(Website)/+page.svelte
@@ -1,4 +1,4 @@
 <main class="m-8">
-<h1 class="font-bold text-3xl mb-8">Welcome to Svelte Cabins Resort</h1>
-<p>Your Mountain Paradise</p>
+<h1 class="font-bold text-3xl mb-8">Welcome to Svelte Cabins</h1>
+<p>Mountain Resort Paradise</p>
 </main>
