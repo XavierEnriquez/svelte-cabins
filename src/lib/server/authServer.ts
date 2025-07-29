2942dd886ef27cbaf226e4e04c7c011688134b45
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-07-29 17:56:55 -0500

    Create authServer.ts

---
+++ src/lib/server/authServer.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { sveltekitCookies } from "better-auth/svelte-kit";
+import { getRequestEvent } from "$app/server";
+
+const { BETTER_AUTH_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+
+export const auth = betterAuth({
+    //...
+    secret: BETTER_AUTH_SECRET,
+    plugins: [sveltekitCookies(getRequestEvent)], 
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: GOOGLE_CLIENT_ID as string,
+            clientSecret: GOOGLE_CLIENT_SECRET as string,
+        },
+  }, 
+});
