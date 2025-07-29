b41d1e45f7a1fda15c687ec71cb18d4386090464
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-07-28 20:09:51 -0500

    Update +page.svelte

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import Header from "$lib/ui/Header.svelte";
+
+  
+
+</script>
+<Header />
 <main class="m-8">
 <h1 class="font-bold text-3xl mb-8">Svelte Cabins Admin Page</h1>
+
+
 </main>
