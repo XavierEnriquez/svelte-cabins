2c05a78135086ddee22f2c62ee97db29ad3f67b5
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-07-31 17:07:46 -0500

    Create auth-client.ts

---
+++ src/lib/client/auth-client.ts
@@ -0,0 +1,7 @@
+import { createAuthClient } from "better-auth/svelte"
+
+// If you're using a different base path other than /api/auth make sure to pass the whole URL including the path. (e.g. http://localhost:3000/custom-path/auth)
+export const authClient = createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: "http://localhost:3000"
+})
