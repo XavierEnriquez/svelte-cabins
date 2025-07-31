7dc864943e0d88fe7bfa1e86df0638197ec5ab53
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-07-31 17:07:29 -0500

    Create auth.ts

---
+++ src/convex/auth.ts
@@ -0,0 +1,10 @@
+import type{ Auth } from 'convex/server';
+import type{ Id } from './_generated/dataModel';
+
+export async function getViewerId(ctx: { auth: Auth }) {
+	const identity = await ctx.auth.getUserIdentity();
+	if (identity === null) {
+		return null;
+	}
+	return identity.subject as Id<'users'>;
+}
