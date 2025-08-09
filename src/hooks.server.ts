9107d0bfd16c8a58e720f4298d4d4ba9a37e2e67
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-08 19:16:11 -0500

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,10 @@
-import { auth } from "$lib/server/auth"; // path to your auth file
 import { svelteKitHandler } from "better-auth/svelte-kit";
 import { building } from '$app/environment'
 import { json } from '@sveltejs/kit';
- 
+import { createAuth } from "./lib/auth";
+
+const auth = createAuth;
+
 export async function handle({ event, resolve }) {
 
     const fetchedSession = await auth.api.getSession({
@@ -19,12 +21,13 @@ export async function handle({ event, resolve }) {
         delete event.locals.session;
     }
 
-   if (event.url.pathname.startsWith('/app') || event.route.id?.startsWith('/app')) {
-if (event.locals?.user?.role !== 'admin') {
-            return json({ message:'Unauthorized'}, { status: 401 });
-        }}
+   if (event.url.pathname.startsWith('/(app)/') || event.route.id?.startsWith('/(app)/')) {
+       if (event.locals?.user) {
+           return json({ message:'Unauthorized'}, { status: 401 });
+       }
+   }
 
-        // When opening the inspect window in browser, chrome-devtools makes two calls to
+    // When opening the inspect window in browser, chrome-devtools makes two calls to
 	// Devtools Project Setting and Automatic Workspace Folders, constantly creating a pesky error
 	// during development. This if() statement takes care of it. Remove if want chrome-devtools in production.
 	if (event.url.pathname.startsWith('/.well-known/appspecific/com.chrome.devtools')) {
