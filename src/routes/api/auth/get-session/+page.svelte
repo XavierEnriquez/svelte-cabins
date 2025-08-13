41da416b1f974b8ca90de1aca6e51d5c1bf89902
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-13 15:31:45 -0500

    Create +page.svelte

---
+++ src/routes/api/auth/get-session/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { authClient } from "$lib/auth-client";
+	import Header from "$lib/ui/Header.svelte";
+  const session = authClient.useSession();
+</script>
+    
+<Header />
+<main class="m-8">
+<h1 class="font-bold text-3xl mb-8">API Auth Page</h1>
+<div>
+      {#if $session.data}
+        <div>
+          <p>
+            {$session?.data?.user.name}
+          </p>
+          <button
+            on:click={async () => {
+              await authClient.signOut();
+            }}
+          >
+            Sign Out
+          </button>
+        </div>
+      {:else}
+        <p>
+          {$session.error
+            ? "Error: " + $session.error.message
+            : "You are not signed in."}
+        </p>
+      {/if}
+    </div>
+</main>
