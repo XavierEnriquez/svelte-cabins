732f72786a544516f1d419487f9e45b09cef780e
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-13 15:32:12 -0500

    Update http.ts

--- src/convex/http.ts
+++ src/convex/http.ts
@@ -2,6 +2,7 @@ import { httpRouter } from 'convex/server';
 import { betterAuthComponent } from './auth';
 import { createAuth } from '../lib/auth';
 
+
 const http = httpRouter();
 
 betterAuthComponent.registerRoutes(http, createAuth);
