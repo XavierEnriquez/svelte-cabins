c8a5da446b90826646adacb267547eaadb720674
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-13 15:31:54 -0500

    Update +page.svelte

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
-    
+  // import { authClient } from "$lib/auth-client";
+	// import Header from "$lib/ui/Header.svelte";
+  // const session = authClient.useSession();
 </script>
-
+    
 <main class="m-8">
 <h1 class="font-bold text-3xl mb-8">Welcome to Svelte Cabins</h1>
 <p>Mountain Resort Paradise</p>
+<!-- <div>
+      {#if $session.data}
+        <div>
+          <p>
+            {$session?.data?.user.name}
+          </p>
+          <button
+            on:click={async () => {
+              await authClient.signOut();
+            }}
+          >
+            Sign Out
+          </button>
+        </div>
+      {:else}
+        <p>
+          {$session.error
+            ? "Error: " + $session.error.message
+            : "You are not signed in."}
+        </p>
+      {/if}
+    </div> -->
 </main>
