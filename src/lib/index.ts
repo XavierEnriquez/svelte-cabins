53046a39e2e9e805b4e2431de87f4e1a365574d1
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-14 14:46:04 -0500

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-import Header from "$lib/ui/Header.svelte";
+import Header from "$lib/components/Header.svelte";
 
 // place files you want to import through the `$lib` alias in this folder.
 export default Header;
