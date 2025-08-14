2bd976f370dc6f79a455f1298573c0844cc9bd90
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-14 14:45:16 -0500

    Create +page.server.ts

---
+++ src/routes/auth/signup/+page.server.ts
@@ -0,0 +1,47 @@
+import { authClient } from "$lib/auth-client";
+import type { PageServerLoad, Actions } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+	const user = await event.locals.user;
+	return { user };
+};
+
+export const actions: Actions = {
+	signin: async ({ request }) => {
+		const formData = await request.formData();
+		const email = formData.get("email");
+		const password = formData.get("password");
+
+		if (typeof email !== "string" || typeof password !== "string") {
+			return { error: "Invalid input" };
+		}
+
+		const user = await authClient.signIn.email({ email, password })
+		if (!user) {
+			return { error: "Invalid email or password" };
+		}
+
+		return { user };
+	},
+    signup: async (event) => {
+		const formData = await event.request.formData();
+        const name = formData.get("name");
+		const email = formData.get("email");
+		const password = formData.get("password");
+
+		if (typeof name !== "string" || typeof email !== "string" || typeof password !== "string") {
+			return { error: "Invalid input" };
+		}
+
+		const user = await authClient.signUp.email({ name, email, password });
+		if (!user) {
+			return { error: "Failed to create user" };
+		}
+
+		return { user };
+	},
+	signOut: async () => {
+		await authClient.signOut();
+		return { success: true };
+	}
+};
