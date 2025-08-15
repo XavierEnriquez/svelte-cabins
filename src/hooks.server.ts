ba6847c7b4f121f56a550c0321b73d6eee45d1d9
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-15 15:48:22 -0500

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,62 +2,56 @@
 import type { Handle } from "@sveltejs/kit";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { getRequestEvent } from "$app/server";
-// import { building } from '$app/environment'
-// import { svelteKitHandler } from "better-auth/svelte-kit";
-// import { createAuth } from "$convex/auth";
-// import { authClient } from "$lib/auth-client";
+import { redirect } from '@sveltejs/kit';
+import { createAuth } from "$convex/auth";
+import {  svelteKitHandler } from "better-auth/svelte-kit";
+import { building } from "$app/environment";
 
-// const auth = createAuth();
-// const auth = authClient;
+const auth = createAuth();
+
 export const handle: Handle = async ({ event, resolve }) => {
   // With the official Convex integration, authentication is handled
   // entirely through Convex, so no special server handling is needed
+
   sveltekitCookies(getRequestEvent);
-  // Fetch current session from Better Auth
-//   const session = await auth.getSession(
-//     {
-//         query: {
-//         disableCookieCache: true,
-//     }, 
-// });
 
-  // Make session and user available on server
-  // if (session) {
-  //   event.locals.session = session ? {
-  //   user: session?.user,
-  //   sessionId: session?.session.id,
-  //   expiresAt: session?.session.expiresAt,
-  //   token: session?.session.token,
-  // } : null;
-  // event.locals.user = session?.user || null;
-  // }
+  const fetchedSession = await auth.api.getSession(
+    {
+      headers: event.request.headers,
+    //     query: {
+    //     disableCookieCache: true,
+    // }, 
+}
+);
+const session = fetchedSession?.session;
+const user = fetchedSession?.user;
 
-  return resolve(event);
+ if (session) {
+        event.locals.user = user;
+        event.locals.session = session ? {
+            user: session.userId,
+            sessionId: session.id,
+            expiresAt: session.expiresAt,
+            token: session.token,
+        } : null;
+    } else {
+        delete event.locals.user;
+        delete event.locals.session;
+    }
+
+  if (event.url.pathname.startsWith('/(app)/') || event.route.id?.startsWith('/(app)/')) {
+       if (!event.locals?.user || !event.locals?.session) {
+        throw redirect(302, '/api/auth/login');
+       }
+   }
+
+  // When opening the inspect window in browser, chrome-devtools makes two calls to
+	// Devtools Project Setting and Automatic Workspace Folders, constantly creating a pesky error
+	// during development. This if() statement takes care of it. Remove if want chrome-devtools in production.
+	if (event.url.pathname.startsWith('/.well-known/appspecific/com.chrome.devtools')) {
+		return new Response(null, { status: 204 }); // Return empty response with 204 No Content
+	}
+
+  return svelteKitHandler({ event, resolve, auth, building });
 };
 
-// export const handle: Handle = async ({ event, resolve }) => {
-//   // With the official Convex integration, authentication is handled
-//   // entirely through Convex, so no special server handling is needed
-//   sveltekitCookies(getRequestEvent);
-//   // Fetch current session from Better Auth
-//   const session = await auth.api.getSession(
-//     {
-//         query: {
-//         disableCookieCache: true,
-//     }, 
-//     headers: event.request.headers
-// });
-
-//   // Make session and user available on server
-//   if (session) {
-//     event.locals.session = session ? {
-//     user: session?.user,
-//     sessionId: session?.session.id,
-//     expiresAt: session?.session.expiresAt,
-//     token: session?.session.token,
-//   } : null;
-//   event.locals.user = session?.user || null;
-//   }
-
-//   return svelteKitHandler({ event, resolve, auth, building });
-// };
