45fe2c5b0b86ed32cdd19c480177aa0a846b3c36
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-08-18 20:35:29 -0500

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,7 @@ import Header from "$lib/ui/Header.svelte";
 
 // place files you want to import through the `$lib` alias in this folder.
 export default Header;
+
+export { authClient } from "$lib/auth-client";
+export { auth } from "$lib/auth";
+export { createAuth } from "$lib/auth";
