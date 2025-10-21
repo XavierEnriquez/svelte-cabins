2ba50b0ae68f14ba471ee4a38de2bd8a67ea3c0c
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-10-21 10:42:14 -0500

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,10 +5,10 @@ import { createAuthClient } from "better-auth/svelte"; // Use svelte instead of 
 // import { browser } from '$app/environment';
 
 export const authClient = createAuthClient({
-  baseURL: browser ? window.location.origin : "http://localhost:5173/",
+  // baseURL: browser ? window.location.origin : "http://localhost:5173/",
   // baseURL: "http://localhost:5173/api/auth",
   // baseURL: import.meta.env.VITE_BETTER_AUTH_URL || "http://localhost:5171",
-  // baseURL: browser ? window.location.origin : 'http://127.0.0.1:5173',
+  baseURL: browser ? window.location.origin : 'http://127.0.0.1:5173',
   // Note: The Convex Better Auth plugins might not work directly with Svelte
   // You may need to handle auth differently in SvelteKit
   plugins: [convexClient(),]
